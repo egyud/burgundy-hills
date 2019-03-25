10117e2053a4d2845842f4e7d7d7b7f13201e830
Andy <andy.egyud@gmail.com>
2019-03-25 13:45:11 -0700

    added mongoose connection to app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const path = require('path');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const db = require('./config/keys').mongoURI;
 
 const userRoutes = require('./routes/user');
 
@@ -12,6 +15,10 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(userRoutes);
+
+mongoose.connect(db, {useNewUrlParser: true})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err));
 
 app.use((req, res, next) => {
     res.status(404).render('404page');
