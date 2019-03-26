f6d2dcc023813ff4785a219b6172bb5f16ad82d4
Andy <andy.egyud@gmail.com>
2019-03-26 11:31:59 -0700

    Fixed typo in register validation file

--- validation/register.js
+++ validation/register.js
@@ -8,7 +8,7 @@ module.exports = ValidateRegisterInput = (data) => {
   data.fullName = !isEmpty(data.fullName) ? data.fullName : "";
   data.email = !isEmpty(data.email) ? data.email : "";
   data.password = !isEmpty(data.password) ? data.password : "";
-  data.password2 = !isEmpty(data.password2) : data.password2 : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
   if (Validator.isEmpty(data.email)) {
     errors.push('Email address is required')
