007ebca42127d935ea9a57dc6016c4befa61c377
Andy <andy.egyud@gmail.com>
2019-04-20 10:35:13 -0700

    Minor changes in prep for deployment

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
-const flash = require('connect-flash');
 
 
+const PORT = process.env.PORT || 5000;
 const db = require('./config/keys').mongoURI;
 
 const homeRoutes = require('./routes/home');
@@ -34,9 +34,6 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(flash());
-
-
 app.use(homeRoutes);
 
 app.use('/api/users', userRoutes);
@@ -49,4 +46,4 @@ app.use((req, res, next) => {
     res.status(404).render('404page', {path: '', pageTitle: '404 Page Not Found'});
 });
 
-app.listen(5000, () => console.log('Server started on port 5000'));
+app.listen(PORT, () => console.log('Server started on port 5000'));
